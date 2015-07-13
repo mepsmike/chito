a3b711017e2d168e2be730e00d66678872a8fb0c
George Chang <georgec1104@gmail.com>
2015-07-13 20:38:07 +0800

    added category model and built connection among restaurant, mrt and category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713115322) do
+ActiveRecord::Schema.define(version: 20150713123257) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "mrts", force: :cascade do |t|
     t.string   "name"
