f8b2576e45894e5f231848dd32865540b123739a
George Chang <georgec1104@gmail.com>
2015-07-16 23:00:01 +0800

    attempted to use user facebook email as login method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
 
 
   def self.from_omniauth(auth_hash)
-    user = where( :fb_uid => auth_hash[:uid] ).first_or_initialize
-    user.name = auth_hash[:info][:name]
+      user = where( :fb_uid => auth_hash[:uid] ).first_or_initialize
+      user.name = auth_hash[:info][:name]
+      user.email = auth_hash[:info][:email]
 
-    user.email = auth_hash[:info][:email]
+      user.fb_token = auth_hash[:credentials][:token]
+      user.fb_expires_at = Time.at(auth_hash[:credentials][:expires_at])
+      user.save!
+      user
 
-    user.fb_token = auth_hash[:credentials][:token]
-    user.fb_expires_at = Time.at(auth_hash[:credentials][:expires_at])
-    user.save!
-    user
   end
 
 
