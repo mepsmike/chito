c326e6d1603807bc180078140a02da08e1cf66c4
George Chang <georgec1104@gmail.com>
2015-07-20 15:30:04 +0800

    added user profile picture from facebook to user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
       user = where( :fb_uid => auth_hash[:uid] ).first_or_initialize
       user.name = auth_hash[:info][:name]
       user.email = auth_hash[:info][:email]
-
+      user.image = auth_hash[:info][:image]
       user.fb_token = auth_hash[:credentials][:token]
       user.fb_expires_at = Time.at(auth_hash[:credentials][:expires_at])
       user.save!
