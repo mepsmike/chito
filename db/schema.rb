38bdb4fe99e8aebc931ca89d5ca6e88631916a59
George Chang <georgec1104@gmail.com>
2015-07-21 18:56:24 +0800

    created post model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719130339) do
+ActiveRecord::Schema.define(version: 20150721105310) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20150719130339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "address"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "restaurants", force: :cascade do |t|
